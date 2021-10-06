91c26639ec93c80ac9674a1a228a482f8dabb0bd
LizMaestracci <liztoire@gmail.com>
2021-10-06 14:51:23 +0200

    prout prout

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './Quizz';
-import './index.css';
+import App from './Quizz/Quizz';
+import './Quizz/Quizz.css';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
